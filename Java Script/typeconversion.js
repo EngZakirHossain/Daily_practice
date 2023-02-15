dde615343b9eae11bb46f561b54147c037da74a9
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-02-15 12:52:49 +0600

    Type Conversion

---
+++ Java Script/typeconversion.js
@@ -0,0 +1,16 @@
+//data type conversion
+//string to number
+let pie = Math.PI
+
+// console.log(typeof(pie), typeof(Number(pie)),Number(""));
+console.log(pie, parseFloat(pie), parseInt(pie));
+
+let x = 1.35698745;
+console.log(x, x.toPrecision(5));
+
+//convertinf boolen to string 
+console.log(false, String(false), false.toString());
+
+//converting boolen to number
+
+console.log(Number(true));
