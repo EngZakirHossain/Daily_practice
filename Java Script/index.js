31c5e23181c11ad3504a5bf84e27862f7cc11ab4
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-02-15 10:10:20 +0600

    Variables - Var, let and const

---
+++ Java Script/index.js
@@ -0,0 +1,13 @@
+/*Variables 
+1.Var //can reassign veriable
+2.let //can reassign veriable
+3.const //can't reassign veriable
+*/
+
+var name = "Zakir Hossain";
+var age = 35;
+let intro = "Vue js 3"
+const pie = 3.1415;
+
+pie = 35;
+console.log(name,age,intro,pie);
