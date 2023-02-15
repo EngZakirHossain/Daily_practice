a75b868258e408d47955a165a629e5d58e615bb9
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-02-15 13:10:44 +0600

    Math Object and Numbers Methods

---
+++ Java Script/mathonjectandnumber.js
@@ -0,0 +1,21 @@
+//math object
+
+let x = 4.65;
+
+//round, Nearest interger number
+//ceil, Top interger number
+//floor, lowest interger number
+//trunc romoving all fraction value
+
+console.log(Math.round(x), Math.ceil(x), Math.floor(x), Math.trunc(x));
+
+//power of 
+let y = 2;
+console.log(Math.pow(2, y));
+//min or max number
+console.log(Math.min(5, 9, 6, 5, 8, 7));
+
+//Random number
+
+console.log(Math.round(Math.random()*10000));
+
