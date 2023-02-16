669c2bae836462bbb02ac3a05adc9a33f337198e
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-02-16 11:56:01 +0600

    function js

---
+++ Java Script/functions.js
@@ -0,0 +1,18 @@
+//ES6 function and  arrow functions
+
+// let x = 6;
+// let y = 5;
+
+// console.log(x + y);
+
+// function add(x,y) {
+//   return x+y
+// }
+// console.log(add(2, 9));
+
+//arrow function
+
+let add = (x,y) => {
+  x + y
+}
+console.log(add(2,9));
