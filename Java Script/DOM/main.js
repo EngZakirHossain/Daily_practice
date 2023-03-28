078616c6d4d5fc60a5b09fc951b8878a17834289
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-03-28 13:12:02 +0600

    using looping and selector

--- Java Script/DOM/main.js
+++ Java Script/DOM/main.js
@@ -1,20 +1,30 @@
 
-//document.getElementbyId();
+// //document.getElementbyId();
 
-let val = document.getElementById('title');
+// let val = document.getElementById('title');
 
-console.log(val);
+// console.log(val);
 
-//get element items details
+// //get element items details
 
-console.log(val.id);
-console.log(val.className);
+// console.log(val.id);
+// console.log(val.className);
 
-//change style
-val.style.color = '#0891b2';
-document.getElementById('title').style.textAlign ='Center' 
+// //change style
+// val.style.color = '#0891b2';
+// document.getElementById('title').style.textAlign ='Center' 
 
-//change content
-document.getElementById('title').innerText = 'Task Mangement'
+// //change content
+// document.getElementById('title').innerText = 'Task Mangement'
 
-document.querySelector('li:nth-child(odd)').style.color='red'
+// document.querySelector('li:nth-child(odd)').style.color = 'red'
+
+const listItems = document.querySelectorAll('.list-group-item')
+console.log(listItems);
+
+listItems.forEach((item, index) => {
+  console.log(item.className, item.index);
+  item.textContent = `${index+1} Zakir Hossain`;
+}); 
+
+// console.log(listItems);
