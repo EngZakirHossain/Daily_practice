06c6ad9f7089d2dd0dd8ab96c7ba6473abc43f53
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-03-28 10:53:10 +0600

    inheritance of class

---
+++ Java Script/opp-2.js
@@ -0,0 +1,27 @@
+class Car{
+  constructor(brand){
+    this.brand = brand;
+  }
+
+  getInfo=()=>{
+    return 'This Car Brand Name Is' + ' ' + this.brand;
+  }
+}
+
+class CarModel extends Car{
+  constructor(brand , model) {
+    super(brand);
+    this.model = model;
+  }
+  printInfo = () => {
+    console.log(this.getInfo() + ' and Its model number is ' + this.model);
+  }
+}
+
+const mazda = new CarModel('mazda', 'Rx8');
+const BMW = new CarModel('BMW', 'R1000M');
+const lancer = new CarModel('lancer', 'Lx800');
+
+mazda.printInfo();
+BMW.printInfo();
+lancer.printInfo();
