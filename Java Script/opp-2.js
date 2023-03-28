375bd3c566128a5df12f40a4185621da997afa2a
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-03-28 10:59:44 +0600

    Static Method in OOP JS

--- Java Script/opp-2.js
+++ Java Script/opp-2.js
@@ -16,6 +16,10 @@ class CarModel extends Car{
   printInfo = () => {
     console.log(this.getInfo() + ' and Its model number is ' + this.model);
   }
+
+  static getFullModelName = () => {
+    return 'This is a Static Method';
+  }
 }
 
 const mazda = new CarModel('mazda', 'Rx8');
@@ -25,3 +29,4 @@ const lancer = new CarModel('lancer', 'Lx800');
 mazda.printInfo();
 BMW.printInfo();
 lancer.printInfo();
+console.log(CarModel.getFullModelName());
