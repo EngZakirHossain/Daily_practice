7196754a937b6a27bc38adf0e114316ebb109032
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-04-13 15:57:03 +0600

    Vue js start and setup first project

---
+++ Java Script/Vue/first_project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ courseName }}</h1>
+  <button @click="count--"> - </button>  
+  {{ count }}
+  <button @click="count++"> + </button>  
+</template>
+
+ <script setup>
+ import { ref } from 'vue';
+  
+  const courseName = ref("Master Vue Js 3");
+  const mentor = ref("Zakir Hossain");
+  let count = ref(0);
+     
+ </script>
